bc18f4e22ee3ea1800f739589f9c4db941926a86
Ahmed-P-Mostafa <ahmed.mostfa.ghorab@gmail.com>
2022-08-11 11:27:20 +0200

    before and after

--- app/src/androidTest/java/com/polotika/androidunittesting/StringComparatorTest.kt
+++ app/src/androidTest/java/com/polotika/androidunittesting/StringComparatorTest.kt
@@ -4,25 +4,18 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import com.google.common.truth.Truth.assertThat
 
-import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class StringComparatorTest {
 
     private lateinit var comparator : StringComparator
-    private lateinit var context: Context
 
     @Before
     fun setUp() {
         comparator = StringComparator()
-        //context = ApplicationProvider.getApplicationContext()
     }
 
-    @After
-    fun tearDown() {
-
-    }
 
     @Test
     fun stringResourceWithSameString_returnsTrue(){
