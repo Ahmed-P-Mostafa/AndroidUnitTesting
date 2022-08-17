e939f83d343559c4ed7c2a6feadb609dd715c13f
Ahmed-P-Mostafa <ahmed.mostfa.ghorab@gmail.com>
2022-08-17 23:11:06 +0200

    testing add new item button respond

--- app/src/androidTest/java/com/polotika/androidunittesting/ui/AddShoppingItemFragmentTest.kt
+++ app/src/androidTest/java/com/polotika/androidunittesting/ui/AddShoppingItemFragmentTest.kt
@@ -1,14 +1,21 @@
 package com.polotika.androidunittesting.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.polotika.androidunittesting.R
+import com.polotika.androidunittesting.data.local.ShoppingItem
+import com.polotika.androidunittesting.getOrAwaitValue
 import com.polotika.androidunittesting.launchFragmentInHiltContainer
+import com.polotika.androidunittesting.repositories.FakeDataRepositoryAndroidTest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -26,6 +33,9 @@ class AddShoppingItemFragmentTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun setup() {
@@ -55,5 +65,19 @@ class AddShoppingItemFragmentTest {
 
     }
 
+    @Test
+    fun pressInsertButtonWithValidInput_insertNewItemToDb(){
+        val testViewModel = ShoppingViewModel(FakeDataRepositoryAndroidTest())
+        launchFragmentInHiltContainer<AddShoppingItemFragment> { viewModel = testViewModel }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("Shopping Item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+        assertThat(testViewModel.shoppingItems.getOrAwaitValue())
+            .contains(ShoppingItem(name = "Shopping Item", price = 5f, amount = 5, imageUrl = ""))
+
+    }
 
 }
--- app/src/main/java/com/polotika/androidunittesting/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/polotika/androidunittesting/ui/AddShoppingItemFragment.kt
@@ -2,11 +2,16 @@ package com.polotika.androidunittesting.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import com.polotika.androidunittesting.R
+import com.polotika.androidunittesting.utils.Status
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
 
 class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
@@ -16,16 +21,50 @@ class AddShoppingItemFragment : Fragment(R.layout.fragment_add_shopping_item) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        subscribeToObservers()
         ivShoppingImage.setOnClickListener {
-            findNavController().navigate(AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment())
+            findNavController().navigate(
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                name = etShoppingItemName.text.toString(),
+                priceString = etShoppingItemPrice.text.toString(),
+                amountString = etShoppingItemAmount.text.toString()
+            )
         }
 
-        val callback = object : OnBackPressedCallback(true){
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 viewModel.setCurImageUrl("")
                 findNavController().popBackStack()
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, Observer {
+            it.getContentIfNotHandled()?.let { result ->
+                when (result.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(requireView(), "Shopping Item Added", Toast.LENGTH_SHORT).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(requireView(), "An Error Occurred", Toast.LENGTH_SHORT).show()
+                    }
+                    Status.LOADING -> {
+                        /*No - OP*/
+                    }
+                }
+
+            }
+        })
+
+        viewModel.curImageUrl.observe(viewLifecycleOwner) {
+            Glide.with(requireView()).load(it).into(ivShoppingImage)
+        }
     }
 }
